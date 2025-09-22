732c51d5e99c55bea73639b31f7d05754f090ae6
abhishekbharti2 <adjack720@gmail.com>
2025-09-23 03:40:41 +0530

    ok

--- backend/utils/cookieControll.js
+++ backend/utils/cookieControll.js
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === "production" || false;
 export const setAuthCookie = (res, token) => {
     res.cookie("token", token, {
         httpOnly: true,
-        secure: isProd,
-        sameSite: isProd ? "strict" : "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 };
